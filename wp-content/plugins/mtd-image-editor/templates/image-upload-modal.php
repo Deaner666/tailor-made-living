8fad2166e08dcc5b072ab264b911ce6e657c267a
Dave Dean <dave@moortor-design.co.uk>
2014-09-30 16:42:10 +0100

    Trying to get this bloody save thing to work.

--- wp-content/plugins/mtd-image-editor/templates/image-upload-modal.php
+++ wp-content/plugins/mtd-image-editor/templates/image-upload-modal.php
@@ -72,11 +72,13 @@
 	    	onSaveButtonClicked: function() {
 	    		featherEditor.saveHiRes();
 	    		console.log('.saveHiRes() method sent');
-	    		var img = document.getElementById(imageID);
-	        	img.src = newURL;
+	    		featherEditor.save();
 	    	},
 	    	onSave: function(imageID, newURL) {
+	    		var img = document.getElementById(imageID);
+	        	img.src = newURL;
 	        	console.log('Called from onSave function');
+	        	featherEditor.close();
 	    	},
 	    	postUrl: mtd_site_url+'/wp-content/plugins/mtd-image-editor/mtd-hires-image-save.php',
 	    	// onSaveHiRes: function(imgID, newURL) {},

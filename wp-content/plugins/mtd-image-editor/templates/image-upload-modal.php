522fa1f4c7037e4d9bf6c41a2a907bc9c2f3e6a2
Dave Dean <dave@moortor-design.co.uk>
2014-10-02 10:44:26 +0100

    Testing onSaveButtonClicked.

--- wp-content/plugins/mtd-image-editor/templates/image-upload-modal.php
+++ wp-content/plugins/mtd-image-editor/templates/image-upload-modal.php
@@ -69,12 +69,10 @@
 	    	theme: 'light', // Check out our new 'light' and 'dark' themes!
 	    	tools: 'all',
 	    	appendTo: '',
-	    	onSaveButtonClicked: function() {
-	    		featherEditor.saveHiRes();
-	    		console.log('.saveHiRes() method sent from onSaveButtonClicked');
-	    		save();
-	    	},
+	    	// onSaveButtonClicked: function() {},
 	    	onSave: function(imageID, newURL) {
+	    		featherEditor.saveHiRes();
+	    		console.log('.saveHiRes() method sent');
 	    		console.log(mtd_site_url+'/wp-content/plugins/mtd-image-editor/mtd-hires-image-save.php');
 	    		var img = document.getElementById(imageID);
 	        	img.src = newURL;

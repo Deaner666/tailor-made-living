35bb177aa64901798ede2ee4a014ecfe2c6a6453
Dave Dean <dave@moortor-design.co.uk>
2014-10-03 13:52:18 +0100

    Moved back to .

--- wp-content/plugins/mtd-image-editor/mtd-hires-image-save.php
+++ wp-content/plugins/mtd-image-editor/mtd-hires-image-save.php
@@ -9,8 +9,8 @@ require_once( ABSPATH . 'wp-admin/includes/image.php' );
 require_once( ABSPATH . 'wp-admin/includes/file.php' );
 require_once( ABSPATH . 'wp-admin/includes/media.php' );
 
-$url = $_POST['url'];
-$tmp = $_POST['url']; // download_url( $url );
+$url = $_REQUEST['url'];
+$tmp = download_url( $url );
 $post_id = 0;
 $desc = "Edited with Aviary";
 $file_array = array();
